68eb5da643105ca6d645aa9bda69974a8305113d
Cyrus Chan <chan1_cyrus2@Cyrus-Chans-MacBook-Air.local>
2015-05-26 18:41:24 -0700

    update forms for description and placeholder

--- GTR/forum/forms.py
+++ GTR/forum/forms.py
@@ -5,7 +5,12 @@ from .models import Link, Vote
 class LinkForm(forms.ModelForm):
     class Meta:
         model = Link
-        exclude = ("submitter", "rank_score", "agree", "disagree")
+        exclude = ("submitter", "rank_score", "agree", "disagree", "url")
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Fill in your headline'}),
+            'self_description': forms.TextInput(attrs={'placeholder':'e.g. mid 20s/software engineer/$80k-90k salaries/work in bay area'}),
+            'description': forms.TextInput(attrs={'placeholder': 'Describe your work'})
+        }
 
 
 class VoteForm(forms.ModelForm):
