282d6f5c48c4c77f390bde223f2a4a77a4c312e0
Cyrus Chan <chan1_cyrus2@Cyrus-Chans-MacBook-Air.local>
2015-05-27 23:05:48 -0700

    updte add post form css

--- GTR/forum/forms.py
+++ GTR/forum/forms.py
@@ -7,9 +7,9 @@ class LinkForm(forms.ModelForm):
         model = Link
         exclude = ("submitter", "rank_score", "agree", "disagree", "url")
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Headline', 'class':'form-control', 'required':''}),
-            'self_description': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Self description (e.g. mid 20s/software engineer/$80k-90k salaries/work in bay area)'}),
-            'description': forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Tell us your story', 'required':''})
+            'title': forms.TextInput(attrs={'placeholder': 'Headline', 'class':'form-control link-form-control', 'required':''}),
+            'self_description': forms.TextInput(attrs={'class':'form-control link-form-control', 'placeholder':'Self description (e.g. mid 20s/software engineer/$80k-90k salaries/work in bay area)'}),
+            'description': forms.Textarea(attrs={'class':'form-control link-form-control', 'placeholder': 'Tell us your story', 'required':''})
         }
 
 
