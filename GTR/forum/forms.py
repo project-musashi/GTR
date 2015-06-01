4a29d2f468d7cda6a23f5a8fd98526f9df42cbca
project-musashi <project.musashi@gmail.com>
2015-05-31 18:00:01 -0700

    Slogan

--- GTR/forum/forms.py
+++ GTR/forum/forms.py
@@ -7,8 +7,8 @@ class LinkForm(forms.ModelForm):
         model = Link
         exclude = ("submitter", "rank_score", "agree", "disagree", "url")
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Headline', 'class':'form-control link-form-control', 'required':''}),
-            'self_description': forms.TextInput(attrs={'class':'form-control link-form-control', 'placeholder':'Self description (e.g. mid 20s/software engineer/$80k-90k salaries/work in bay area)'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Title', 'class':'form-control link-form-control', 'required':''}),
+            'self_description': forms.TextInput(attrs={'class':'form-control link-form-control', 'placeholder':'Self Description (e.g. Mid 20s/Software Engineer/$80-90k Salary/Bay Area)'}),
             'description': forms.Textarea(attrs={'class':'form-control link-form-control', 'placeholder': 'Tell us your story', 'required':''})
         }
 
